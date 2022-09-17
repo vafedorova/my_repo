e43f7319b210df1e5f78ce8a9255930ecfccbfc4
varvara <va.fedorova2006@gmail.com>
2022-09-17 22:51:22 +0300

    show results on the screen

--- main.cpp
+++ main.cpp
@@ -4,16 +4,29 @@
 using namespace std;
 
 int main() {
-  cout << "Введите Ваше имя: ";
+  cout << "Введите имя: ";
   string name;
   cin >> name;
-  cout << "Введите Вашу фамилию: ";
+  cout << "Введите фамилию: ";
   string surname;
   cin >> surname;
-  cout << "Введите Вашу любимую книгу: ";
-  string book;
-  cin >> book;
+  cout << "Введите любимый фильм: ";
+  string film;
+  getline(cin, film);
+  getline(cin, film);
   string file_name = "results.txt";
   ofstream fout(file_name, ios::app);
-  fout << name << " " << surname << " " << book << endl;
+  fout << name << " " << surname << " " << film << endl;
+  fout.close();
+  cout << "Вывести на экран содержимое файла? (Yes/No): ";
+  string answer;
+  cin >> answer;
+  if (answer == "No" || answer == "no" || answer == "N" || answer == "n") {
+    return 0;
+  }
+  else {
+    ifstream fin(file_name);
+    cout << fin.rdbuf() << endl;
+    fin.close();
+  }
 }
