be8251efaf64090ce7b2816769371e68ba6bce9d
varvara <va.fedorova2006@gmail.com>
2022-09-17 22:58:03 +0300

    choice added

--- main.cpp
+++ main.cpp
@@ -4,27 +4,26 @@
 using namespace std;
 
 int main() {
-  cout << "Введите имя: ";
-  string name;
-  cin >> name;
-  cout << "Введите фамилию: ";
-  string surname;
-  cin >> surname;
-  cout << "Введите любимый фильм: ";
-  string film;
-  getline(cin, film);
-  getline(cin, film);
+  cout << "Провести опрос (1) или вывести сохраненные результаты (2): ";
+  string choice;
+  cin >> choice;
   string file_name = "results.txt";
   ofstream fout(file_name, ios::app);
-  fout << name << " " << surname << " " << film << endl;
-  fout.close();
-  cout << "Вывести на экран содержимое файла? (Yes/No): ";
-  string answer;
-  cin >> answer;
-  if (answer == "No" || answer == "no" || answer == "N" || answer == "n") {
-    return 0;
+  if (choice == "1") {
+    cout << "Введите имя: ";
+    string name;
+    cin >> name;
+    cout << "Введите фамилию: ";
+    string surname;
+    cin >> surname;
+    cout << "Введите любимый фильм: ";
+    string film;
+    getline(cin, film);
+    getline(cin, film);
+    fout << name << " " << surname << " " << film << endl;
+    fout.close();
   }
-  else {
+  else if (choice == "2") {
     ifstream fin(file_name);
     cout << fin.rdbuf() << endl;
     fin.close();
