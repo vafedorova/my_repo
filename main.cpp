2e1e86efb0172496d4163f8ad5cc9d744714c899
varvara <va.fedorova2006@gmail.com>
2022-09-17 21:53:30 +0300

    read questions from file feature added

--- main.cpp
+++ main.cpp
@@ -4,16 +4,19 @@
 using namespace std;
 
 int main() {
-  cout << "Введите Ваше имя: ";
-  string name;
-  cin >> name;
-  cout << "Введите Вашу фамилию: ";
-  string surname;
-  cin >> surname;
-  cout << "Введите Вашу любимую книгу: ";
-  string book;
-  cin >> book;
+  string questions_file_name = "questions.txt";
+  ifstream fin(questions_file_name);
   string file_name = "results.txt";
   ofstream fout(file_name, ios::app);
-  fout << name << " " << surname << " " << book << endl;
+  while (!fin.eof()) {
+    string question;
+    getline(fin, question);
+    cout << question;
+    string answer;
+    getline(cin, answer);
+    fout << answer << " ";
+  }
+  fout << endl;
+  fout.close();
+  fin.close();
 }
